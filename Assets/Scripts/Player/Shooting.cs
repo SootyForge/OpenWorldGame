287a7f57a136a95a48d9bcf6c387a262e5b22790
SootyForge <42379545+SootyForge@users.noreply.github.com>
2019-05-09 16:25:57 +1000

    005 - Bullets 'n' Guns Galore
    Lots of stuff.

---
+++ Assets/Scripts/Player/Shooting.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.Linq;
+using NaughtyAttributes;
+
+[RequireComponent(typeof(Player))]
+public class Shooting : MonoBehaviour
+{
+    [BoxGroup("Weapon System")] public Weapon currentWeapon;
+    [BoxGroup("Weapon System")] public List<Weapon> weapons = new List<Weapon>();
+    [BoxGroup("Weapon System")] public int currentWeaponIndex = 0;
+
+    private Player player;
+    private CameraLook cameraLook;
+
+    void Awake()
+    {
+        player = GetComponent<Player>();
+        cameraLook = GetComponent<CameraLook>();
+    }
+
+    void Start()
+    {
+        // Get all weapons attached to Player
+        weapons = GetComponentsInChildren<Weapon>().ToList();
+
+        // Select first one
+        SelectWeapon(0);
+    }
+
+    void Update()
+    {
+        // If there is a weapon
+        if (currentWeapon)
+        {
+            bool fire1 = Input.GetButton("Fire1");
+            if (fire1)
+            {
+                // Check if weapon can shoot
+                if (currentWeapon.canShoot)
+                {
+                    // Shoot the weapon
+                    currentWeapon.Shoot();
+                    // Apply Weapon Recoil
+                    Vector3 euler = Vector3.up * 2f;
+                    // Randomize the pitch
+                    euler.x = Random.Range(-1f, 1f);
+                    // Apply offset to camera using weapon recoil
+                    cameraLook.SetTargetOffset(euler * currentWeapon.recoil);
+                }
+            }
+        }
+    }
+
+    void DisableAllWeapons()
+    {
+        // Loop through all weapons
+        foreach (var item in weapons)
+        {
+            // Disable each GameObject
+            item.gameObject.SetActive(false);
+        }
+    }
+
+    void SelectWeapon(int index)
+    {
+        // Check if index is within bounds
+        if (index >= 0 && index < weapons.Count)
+        {
+            // Disable all weapons
+            DisableAllWeapons();
+            // Select currentWeapon
+            currentWeapon = weapons[index];
+            // Enable currentWeapon
+            currentWeapon.gameObject.SetActive(true);
+            // Update currentWeaponIndex
+            currentWeaponIndex = index;
+        }
+    }
+}
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using NaughtyAttributes;
+
+public class Weapon : MonoBehaviour
+{
+    [BoxGroup("Stats")] public int damage = 10, maxReserve = 500, maxClip = 20;
+    [BoxGroup("Stats")] public float spread = 2f, recoil = 1f, range = 10f, shootRate = .2f;
+    [BoxGroup("References")] public Transform shotOrigin;
+    [BoxGroup("References")] public GameObject bulletPrefab;
+
+    [ShowNonSerializedField] public bool canShoot = false;
+
+    [ShowNonSerializedField] private float shootTimer = 0f;
+
+    [ShowNonSerializedField] private int currentReserve = 0, currentClip = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentReserve = maxReserve;
+        currentClip = maxClip;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Count up shoot timer
+        shootTimer += Time.deltaTime;
+        // If shoot timer reaches shoot rate
+        if (shootTimer >= shootRate)
+        {
+            // Can shoot!
+            canShoot = true;
+        }
+    }
+    public void Reload()
+    {
+        // Task:
+        // - Ammo Math
+        // - Auto-Reload when firing
+        if (currentReserve > 0)
+        {
+            if (currentClip >= 0)
+            {
+                currentReserve += currentClip;
+                currentClip = 0;
+
+                if (currentReserve >= maxClip)
+                {
+                    currentReserve -= maxClip - currentClip;
+                    currentClip = maxClip;
+                }
+                else if (currentClip < maxClip)
+                {
+                    int tempMag = currentReserve;
+                    currentClip = tempMag;
+                    currentReserve -= tempMag;
+                } 
+            }
+        }
+    }
+    public void Shoot()
+    {
+        // Reset timer & canShoot to false
+        currentClip--;
+        // Auto-Reload
+        if(currentClip == 0)
+        {
+            Reload();
+        }
+        shootTimer = 0f;
+        canShoot = false;
+
+        // Get some values
+        Camera attachedCamera = Camera.main; // Note (Manny): Pass the reference into weapon somehow
+        Transform camTransform = attachedCamera.transform; // Shortening Camera's Transform to 'camTransform'
+        Vector3 bulletOrigin = camTransform.position; // Starting point of the bullet (Centre of Camera)
+        Quaternion bulletRotation = camTransform.rotation; // Rotation of the bullet
+        Vector3 lineOrigin = shotOrigin.position; // Where the bullet line starts
+        Vector3 direction = camTransform.forward; // Forward direction of camera
+
+        // Spawn Bullet
+        GameObject clone = Instantiate(bulletPrefab, bulletOrigin, bulletRotation);
+        Bullet bullet = clone.GetComponent<Bullet>();
+        bullet.Fire(lineOrigin, direction);
+    }
+}
