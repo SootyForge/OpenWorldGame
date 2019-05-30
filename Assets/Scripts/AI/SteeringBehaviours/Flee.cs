a9a06da2fd6c5f24c8a4d7c22584c05c5cddeac4
SootyForge <42379545+SootyForge@users.noreply.github.com>
2019-05-30 12:09:12 +1000

    010 - AI Overhaul 1
    AI Behaviour Trees.

---
+++ Assets/Scripts/AI/SteeringBehaviours/Flee.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using NaughtyAttributes;
+
+[CreateAssetMenu(fileName = "Flee", menuName = "SteeringBehaviours/Flee", order = 1)]
+public class Flee : SteeringBehaviour
+{
+  public float stoppingDistance = 1f; // Requires that you set "NavMeshAgent.stoppingDistance" to zero
+
+  public override void OnDrawGizmosSelected(AI owner)
+  {
+    Gizmos.color = Color.red;
+    float distance = Vector3.Distance(owner.target.position, owner.transform.position);
+    Gizmos.DrawWireSphere(owner.target.position, stoppingDistance - distance);
+  }
+
+  public override Vector3 GetForce(AI owner)
+  {
+    // Create a value to return later
+    Vector3 force = Vector3.zero;
+
+    if (owner.hasTarget) // target != null
+    {
+      // Get direction from AI agent to Target
+      force += owner.transform.position - owner.target.position;
+    }
+
+    // Return normalized value
+    return force.normalized;
+  }
+}
---
+++ Assets/Scripts/AI/SteeringBehaviours/Seek.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using NaughtyAttributes;
+
+[CreateAssetMenu(fileName = "Seek", menuName = "SteeringBehaviours/Seek", order = 1)]
+public class Seek : SteeringBehaviour
+{
+  public float stoppingDistance = 1f; // Requires that oyu set "NavMeshAgent.stoppingDistance" to zero
+
+  public override void OnDrawGizmosSelected(AI owner)
+  {
+    Gizmos.color = Color.blue;
+    float distance = Vector3.Distance(owner.target.position, owner.target.position);
+    Gizmos.DrawWireSphere(owner.transform.position, distance - stoppingDistance);
+  }
+
+  public override Vector3 GetForce(AI owner)
+  {
+    // Create a value to return later
+    Vector3 force = Vector3.zero;
+
+    if(owner.hasTarget) // target != null
+    {
+      // Get direction from AI agent to Target
+      force += owner.target.position - owner.transform.position;
+    }
+
+    // Return normalized value
+    return force.normalized;
+  }
+}
